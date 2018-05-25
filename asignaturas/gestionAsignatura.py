51fb3377329abba7be857c1f89f496b3e4d65db6
Gustavo Alejandro Castellanos <gustavoaca1997@gmail.com>
2018-05-24 23:11:09 -0400

    Se implementa la vista EliminarAsignaturaView que soporta métodos POST para eliminar asignaturas
    Pero necesita de que se pase un parametro '_method'='delete'

--- asignaturas/gestionAsignatura.py
+++ asignaturas/gestionAsignatura.py
@@ -1,4 +1,4 @@
-from .models import Asignatura
+from asignaturas.models import Asignatura
 # En este script se encuentra las funciones para gestionar la tabla de 
 # profesores 
 #       - Anade 
@@ -9,5 +9,5 @@ def eliminarAsignatura(codasig):
     try:
         delete_return = Asignatura.objects.get(codasig=codasig).delete()
         print('Se borraron {} asignaturas: {}'.format(delete_return[0], delete_return[1]))
-    except DoesNotExist as e:
+    except Exception as e:
         print(e)
--- asignaturas/urls.py
+++ asignaturas/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import *
 
 app_name = 'asignaturas'
 urlpatterns = [
     path('', index, name='index'),
+    path('eliminar-asignatura/', EliminarAsignaturaView.as_view(), name='eliminar-asignatura')
 ]
