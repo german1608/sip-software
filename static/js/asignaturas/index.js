4908877c3ed1517f6431049401545ba385b50fbe
Gustavo Alejandro Castellanos <gustavoaca1997@gmail.com>
2018-05-26 19:48:01 -0400

    Se crea un botón en base/blanco.html para ir al dashboard fácil

--- static/js/asignaturas/index.js
+++ static/js/asignaturas/index.js
@@ -2,3 +2,7 @@ $(document).ready( function () {
     // Inicializar Datable para asignaturas
     $('#t_asignaturas').DataTable();
 } );
+
+$('.eliminar-asignatura-btn').on('click', () => {
+    alert('Este botón se implementará luego de que la Datatable muestre data real de la base de datos.')
+})
