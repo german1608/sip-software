fb11fbbc980ae21511dac6e4a736ff97630d94d1
DanielRS <danielrs9705@gmail.com>
2018-05-27 20:25:58 -0400

    Se agrega los modelos al admin

--- asignaturas/admin.py
+++ asignaturas/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Asignatura, Horario, ProgramaAsignatura
 # Register your models here.
+
+admin.site.register(Asignatura)
+admin.site.register(Horario)
+admin.site.register(ProgramaAsignatura)
