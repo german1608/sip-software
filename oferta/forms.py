4abbd52da573a91e75ed1c3850325498292e5ce3
German Robayo Paz <germanrobayo33@gmail.com>
2018-06-14 16:57:51 -0400

    Quita clean de forms.py

--- oferta/forms.py
+++ oferta/forms.py
@@ -8,11 +8,3 @@ class OfertaForm(forms.ModelForm):
     class Meta:
         model = Oferta 
         fields = '__all__'
-
-    def clean(self):
-        cleaned_data = super().clean()
-        trimestre = cleaned_data.get('trimestre')
-        anio = cleaned_data.get('anio')
-        coordinacion = cleaned_data.get('coordinacion')
-
-
